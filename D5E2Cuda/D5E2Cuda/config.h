ab40a3a8dfd94a906258ac74337d877a646ba12f
Christoph Schleßelmann <c.schlesselmann@googlemail.com>
2016-12-12 13:44:10 +0100

    Added Cuda Project

---
+++ D5E2Cuda/D5E2Cuda/config.h
@@ -0,0 +1,14 @@
+﻿#ifdef USEGPU
+#define GPU_THREADS 1
+#endif
+
+#ifndef GPU_THREADS
+#define GPU_THREADS 1
+#endif
+
+#ifndef THREADS
+#define THREADS 3
+#endif
+
+#undef I_SIZE
+#define I_SIZE 15
